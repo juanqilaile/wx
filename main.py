f9fef471fd3398ee0ea213e570f309aaa3bba1ac
juanqilaile <92581862+juanqilaile@users.noreply.github.com>
2022-08-23 06:52:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def get_count():
 
 def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < ZhDate(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day):
+  if next < (ZhDate(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day).to_datetime()):
     next = next.replace(year=next.year + 1)
-  return (next - ZhDate(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day)).days
+  return (next - (ZhDate(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day).to_datetime()).days
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
